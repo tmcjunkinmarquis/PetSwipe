64d1e3767b5ef9564b63130985a7ce628a4e5199
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-09-05 15:20:08 -0600

    Initial commit

--- PetSwipe/ViewController.swift
+++ PetSwipe/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var label: UILabel!
+    @IBAction func Button(_ sender: Any) {
+        label.text = "Awesome Label"
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
